da9eff2d71fb5966179f1155f9fafc5d7ccd8f96
pi <pi@wafersat.com>
2018-07-02 15:44:27 -0400

    fan tests, PI then PD

--- dark_control.py
+++ dark_control.py
@@ -408,7 +408,7 @@ def davefilter(avg_temp, a=.3, delta_t=.5):
 try:
     print('trial started')
     #filename, goal temp, delta, seconds to run with heat, control_alg, 
-    Qi_track(34, 2, 300, PI, cool_down=True)
+    Qi_track(34, 2, 300, PD, cool_down=True)
 except KeyboardInterrupt:
     print ('\n')
 finally:
