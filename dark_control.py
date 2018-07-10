5811168bf78a8b63925d66e7946888294b5f3c1a
kpodsada <kpodsada@imsa.edu>
2018-07-10 16:32:44 -0400

    karolina is testing gitbash

--- dark_control.py
+++ dark_control.py
@@ -433,7 +433,7 @@ def davefilter(avg_temp, a=.3, delta_t=.5):
 try:
     print('trial started')
     #filename, goal temp, delta, seconds to run with heat, control_alg, 
-    Qi_track(20, 2, 500, PID, cool_down=True)
+    Qi_track(20, 2, 600, PID, cool_down=True)
 except KeyboardInterrupt:
     print ('\n')
 finally:
